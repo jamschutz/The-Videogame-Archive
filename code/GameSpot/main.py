a620a17a20f5ab4e0ec87b321912d8dfc7895468
Joey Schutz <schutzjoey@gmail.com>
2023-02-19 14:36:45 -0500

    parse datetime from date

--- code/GameSpot/main.py
+++ code/GameSpot/main.py
@@ -1,8 +1,12 @@
 import web_scraper as ws
-import json
+import json, time
+from pathlib import Path
+from datetime import datetime
 
 BASE_URL = 'https://www.gamespot.com'
 START_PAGE_NEWS = 4425
+END_PAGE_NEWS   = 4424
+DATETIME_FORMAT = '%A, %b %d, %Y %I:%M%p'
 
 TEST_URL = 'https://www.gamespot.com/articles/miyamoto-talks-dolphin-at-space-world-99/1100-2323742/'
 
@@ -10,30 +14,50 @@ TEST_URL = 'https://www.gamespot.com/articles/miyamoto-talks-dolphin-at-space-wo
 def get_article_links(page_number, proxies):
     while len(proxies) > 0:
         try:
-            article_links = ws.get_links_from_news_page(START_PAGE_NEWS, proxies[0])
+            article_links = ws.get_links_from_news_page(page_number, proxies[0])
             return article_links
 
         except Exception as e:
             """ just keep trying proxies """
-            print(f'{str(e)}\n\n----------trying next proxy...----------')
+            print(f'----------trying next proxy...----------')
             proxies.pop()
 
             if(len(proxies) == 0):
                 print('------------------OUT OF PROXIES!!!----------------')
                 return
 
+            time.sleep(1)
+
 
 def save_sitemap():
     proxies = ws.get_free_proxies()
     sitemap = []
-    sitemap.extend(get_article_links(START_PAGE_NEWS, proxies))
+
+    page = START_PAGE_NEWS
+    while len(proxies) > 0 and page >= END_PAGE_NEWS:
+        print(f'fetching page {str(page)}. proxies remaining: {str(len(proxies))}')
+        sitemap.extend(get_article_links(page, proxies))
+        page -= 1
+        time.sleep(1)
     
-    with open("../../archive/sitemaps/GameSpot.json", "w") as json_file:
+    with open("../../archive/_sitemaps/GameSpot.json", "w") as json_file:
         json.dump(sitemap, json_file)
 
 
-def save_wegpage(url):
+def save_wegpage(url, date, title):
     proxies = ws.get_free_proxies()
+
+    # parse year / month / day from date
+    date_dt = datetime.strptime(date, DATETIME_FORMAT)
+    year = date_dt.strftime("%Y")
+    month = date_dt.strftime('%m')
+    day = date_dt.strftime('%d')
+
+    # build folder path
+    folder_path = f'../../archive/{str(year)}/{str(month)}'
+    filename = f'GameSpot_{str(day)}-{str(month)}-{str(year)}_{title}.html'
+
+    # grab webpage
     for proxy in proxies:
         try:
             webpage = ws.get_webpage(url, proxy)
@@ -42,8 +66,9 @@ def save_wegpage(url):
             print('success!')
 
             # save to file
-            with open("output.html", "w") as text_file:
-                text_file.write(webpage)
+            Path(folder_path).mkdir(parents=True, exist_ok=True) # ensure folder exists
+            with open(f'{folder_path}/{filename}', "w") as html_file:
+                html_file.write(webpage)
 
             # stop searching with proxies
             break
--- code/GameSpot/tester.py
+++ code/GameSpot/tester.py
@@ -1,18 +1,33 @@
+import json, time
+from pathlib import Path
+from datetime import datetime
+
+# def inner_test(arr):
+#     for i in range(5):
+#         arr.pop(0)
 
 
-def inner_test(arr):
-    for i in range(5):
-        arr.pop(0)
+# def test():
+#     arr = []
+#     for i in range(20):
+#         arr.append(i)
+
+#     while len(arr) > 0:
+#         inner_test(arr)
+#         time.sleep(1)
+#         print('going again! current lenght: ' + str(len(arr)))
+
+# test()
 
 
-def test():
-    arr = []
-    for i in range(20):
-        arr.append(i)
+# Path('../../archive/1999').mkdir(parents=True, exist_ok=True)
 
-    while len(arr) > 0:
-        inner_test(arr)
+testdate = datetime.strptime('Sunday, Dec 31, 1987 5:37pm', '%A, %b %d, %Y %I:%M%p')
 
-        print('going again! current lenght: ' + str(len(arr)))
+year = testdate.strftime("%Y")
+month = testdate.strftime('%m')
+day = testdate.strftime('%d')
+print(f'year: {year}, month: {month}, day: {day}')
 
-test()
+
+# print(is_dir)
