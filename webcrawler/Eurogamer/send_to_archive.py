21d8d98daebd4436641109e1dc1dccff692360e2
Joey Schutz <schutzjoey@gmail.com>
2023-02-21 22:53:14 -0500

    indexed eurogamer through 2006

--- webcrawler/Eurogamer/send_to_archive.py
+++ webcrawler/Eurogamer/send_to_archive.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from datetime import datetime
 
 # if you change the below!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-DATA_DUMP_FILE = '../../data/_dumps/Eurogamer_10-1999_12-2003.json'
+DATA_DUMP_FILE = '../../data/_dumps/Eurogamer_01-2004_12-2006.json'
 # DATA_DUMP_FILE_TYPE = 'review'
 # change the above too!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ARCHIVE_FOLDER_PATH = '../../data/_archive/'
