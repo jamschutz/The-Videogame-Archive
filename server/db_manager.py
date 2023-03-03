7d528dff13162b2ab5364937954074dbb81d1647
Joey Schutz <schutzjoey@gmail.com>
2023-03-02 21:06:00 -0500

    created ArchivedWebsites API

--- server/db_manager.py
+++ server/db_manager.py
@@ -23,7 +23,7 @@ def get_db_query(year, month, day, website_id):
     return query
 
 
-@app.route('/', methods=['GET', 'OPTIONS'])
+@app.route('/Article', methods=['GET', 'OPTIONS'])
 @cross_origin(origin='*')
 def get_articles_for_date():
     year = request.args.get('year')
@@ -49,6 +49,53 @@ def get_articles_for_date():
             'day': article[2],
             'url': article[3],
             'website': article[4]
+        })
+
+    response = jsonify(articles_formatted)
+    return response
+
+
+
+@app.route('/ArchivedWebsites', methods=['GET', 'OPTIONS'])
+@cross_origin(origin='*')
+def get_archived_websites():
+    limit = request.args.get('limit') if request.args.get('websiteId') != None else 50
+    website_id = int(request.args.get('websiteId')) if request.args.get('websiteId') != None else -1
+
+    # connect to db, and save
+    db = sqlite3.connect(DATABASE_FILE)
+    cursor = db.cursor()
+
+    # execute script, save, and close
+    query = f"""
+SELECT
+	Title, Url, YearPublished, MonthPublished, DayPublished, WebsiteId
+FROM
+	Article
+WHERE
+	IsArchived = 0 
+    """
+    if website_id > 0:
+        query += f' AND WebsiteId = {website_id}'
+
+    query += f"""
+LIMIT {limit}
+    """
+
+
+    result = cursor.execute(query)
+    articles = result.fetchall()
+    db.close()
+
+    articles_formatted = []
+    for article in articles:
+        articles_formatted.append({
+            'title': article[0],
+            'url': article[1],
+            'year': article[2],
+            'month': article[3],
+            'day': article[4],
+            'website': article[5]
         })
 
     response = jsonify(articles_formatted)
