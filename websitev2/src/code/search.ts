c0e41b9822ca17cfe491f00898abd7c8efbb194f
Joey Schutz <schutzjoey@gmail.com>
2023-10-19 17:54:18 -0400

    search progress bar

--- websitev2/src/code/search.ts
+++ websitev2/src/code/search.ts
@@ -1,6 +1,22 @@
 let se_searchBar = new SearchBar();
 
+function sortByDate(a: Article, b: Article) {
+    if (a.date.toNumber() < b.date.toNumber()) {
+      return -1;
+    }
+    if (a.date.toNumber() > b.date.toNumber()) {
+      return 1;
+    }
+    return 0;
+  }
+
 function showSearchResults(results: Article[]) {
+    // sort articles
+    results.sort(sortByDate);
+
+    // hide progress bar
+    let progressBar = document.getElementById('Search-progressBar');
+    progressBar.style.display = 'none';
     let containerDiv = document.getElementById('Search-resultsContainer');
         
     // if no articles for this day, just say so
@@ -36,6 +52,7 @@ function showSearchResults(results: Article[]) {
         console.log('getting results for: ' + searchRequest.searchTerms);
 
         let results = await DataManager.getSearchResults(searchRequest);
+        console.log('got results');
         let calculationTime = (Date.now() - startTime) / 1000; // milliseconds to seconds
         document.getElementById('Search-resultCount').innerText = `${results.length} results (${calculationTime.toFixed(2)} seconds)`;
         showSearchResults(results);
