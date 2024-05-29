291e95d6a9aec69857301640e2728feb293c7da8
Joey Schutz <schutzjoey@gmail.com>
2024-05-29 10:56:37 -0400

    Insert Articles working

--- serverv2/WebcrawlerAPIs/Core/DB/ArticlesManager.py
+++ serverv2/WebcrawlerAPIs/Core/DB/ArticlesManager.py
@@ -75,14 +75,44 @@ class ArticlesManager:
         # verify info looks correct
         for article in articles:
             if article.author == '' or article.url == ''      or article.type == '' or \
-               article.title == ''  or article.subtitle == '' or article.website_id == None or \
-               article.date == None:
+               article.title == ''or article.website_id == None or article.date == None:
                 
-                raise Exception("article is missing one of the following properties: author, url, type, title, subtitle, websiteid, date. bailing!")
+                raise Exception("article is missing one of the following properties: author, url, type, title, websiteid, date. bailing!")
+            
+        # remove articles that already exist in db (ignore duplicates)
+        existing_articles = self.__get_existing_article_urls(articles)
+        articles_to_insert = []
+        urls_added = set()
+        for a in articles:
+            if a.url not in existing_articles and a.url not in urls_added:
+                articles_to_insert.append(a)
+                urls_added.add(a.url)
+        
+        # if no articles to insert ,just return
+        if len(articles_to_insert) == 0:
+            return
             
         # otherwise, get author and url ids for each article
-        author_ids = self.writers.get_writers_and_create_if_not_exist(articles)
-        type_ids = self.article_types.get_article_types_and_create_if_not_exist(articles)
+        author_ids = self.writers.get_writers_and_create_if_not_exist(articles_to_insert)
+        type_ids = self.article_types.get_article_types_and_create_if_not_exist(articles_to_insert)
+
+        # inject ids into article data, and format title / subtitle, etc
+        articles_to_insert = self.__prep_articles_for_insertion(articles_to_insert, author_ids, type_ids)
+
+        # build query
+        articles_formatted = [
+            f"('{a.title}', '{a.subtitle}', '{a.url}', {a.author_id}, {a.website_id}, {a.date}, {a.type_id}, {a.thumbnail if a.thumbnail is not None else 'NULL'}, false)"
+            for a in articles_to_insert
+        ]
+        query = f"""
+            INSERT INTO "Articles"
+                ("Title", "Subtitle", "Url", "AuthorId", "WebsiteId", "DatePublished", "ArticleTypeId", "Thumbnail", "IsArchived")
+            VALUES
+                {','.join(articles_formatted)}
+        """
+
+        # and run
+        self.db.run_query(query)
 
 
 
@@ -106,8 +136,96 @@ class ArticlesManager:
 
 
 
+    # ==================================================================== #
+    # ============    HELPER METHODS    ================================== #
+    # ==================================================================== #
+
+    def __get_existing_article_urls(self, articles):
+        urls = [f"'{a.url}'" for a in articles]
+        query = f"""
+            SELECT
+                "Url"
+            FROM
+                "Articles"
+            WHERE
+                "Url" IN ({','.join(urls)})
+        """
+        results = self.db.get_query(query)
+
+        existing_urls = set()
+        for url in results:
+            existing_urls.add(url[0])
+
+        return existing_urls
+    
+
+    def __prep_articles_for_insertion(self, articles, author_id_lookup, type_id_lookup):
+        # inject ids into article data
+        for article in articles:
+            article.author_id = author_id_lookup[article.author]
+            article.type_id = type_id_lookup[article.type]
+
+            # clean up title and subtitle
+            article.title = article.title.replace("'", "''")
+            article.subtitle = article.subtitle.replace("'", "''")
+
+            # if subtitle is too long, just truncate and slap an ellipses on it
+            if len(article.subtitle) > 250:
+                truncate_index = 246
+
+                # if we're truncating an apostophe, it will cause string parsing issues...back up until we're safe
+                while article.subtitle[truncate_index] == "'":
+                    truncate_index -= 1
+
+                # and truncate
+                article.subtitle = f"{article.subtitle[:truncate_index]}..."
+
+        return articles
+
+
+
+
 
 if __name__ == '__main__':
     articles_manager = ArticlesManager()
-    articles = articles_manager.get_articles_to_archive(10, 1)
-    print([a.to_string() for a in articles])
+
+    articles = [
+        Article(
+            title="The Abrupt Goodbye",
+            subtitle='',
+            author='Tim',
+            website_id=6,
+            date=20071018,
+            type='UNKNOWN',
+            url="http://indygamer.blogspot.com/2007/10/abrupt-goodbye.html"
+        ),
+        Article(
+            title="European Promotion Tournament recap",
+            subtitle='',
+            author='UNKNOWN',
+            website_id=1,
+            date=20131218,
+            type='news',
+            url="https://www.gamespot.com/articles/european-promotion-tournament-recap/1100-6436643/"
+        ),
+        Article(
+            title='testing article',
+            subtitle='',
+            author='UNKNOWN',
+            website_id=1,
+            date=20240529,
+            type='news',
+            url="www.dummyurl.com"
+        ),
+        Article(
+            title='testing article',
+            subtitle='',
+            author='UNKNOWN',
+            website_id=1,
+            date=20240529,
+            type='news',
+            url="www.dummyurl.com"
+        )
+    ]
+    articles = articles_manager.insert_articles(articles)
+    print(articles)
--- serverv2/WebcrawlerAPIs/Entities/Article.py
+++ serverv2/WebcrawlerAPIs/Entities/Article.py
@@ -1,5 +1,5 @@
 class Article:
-    def __init__(self, id=None, author="", url='', type='', title='', subtitle='', website_id=None, date=None, thumbnail='', tags=[]):
+    def __init__(self, id=None, author="", url='', type='', title='', subtitle='', website_id=None, date=None, thumbnail=None, tags=[], author_id=None, type_id=None):
         self.author = author
         self.url = url
         self.type = type
@@ -10,6 +10,8 @@ class Article:
         self.thumbnail = thumbnail
         self.id = id
         self.tags = tags
+        self.author_id = author_id,
+        self.type_id = type_id
 
 
     def to_string(self):
@@ -17,5 +19,8 @@ class Article:
             'title': self.title,
             'subtitle': self.subtitle,
             'url': self.url,
-            'date': self.date
+            'date': self.date,
+            'author_id': self.author_id,
+            'type_id': self.type_id,
+            'website_id': self.website_id
         }
