cab701d701fc5af842e30a6c555368360ba77c20
jamschutz <52084893+jamschutz@users.noreply.github.com>
2025-04-15 13:15:20 -0400

    for now allow any origin

--- server/DataAPIs/Program.cs
+++ server/DataAPIs/Program.cs
@@ -12,7 +12,8 @@ builder.Services.AddCors(options => {
     options.AddPolicy(
         name: AllowLocalConnections,
         policy => {
-            policy.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost").AllowAnyHeader().AllowAnyMethod();
+            // policy.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost").AllowAnyHeader().AllowAnyMethod();
+            policy.SetIsOriginAllowed(origin => true).AllowAnyHeader().AllowAnyMethod();
         }
     );
 });
