1d4feb1324f7b4907849a0e7ffb29a4d555f80d4
jamschutz <52084893+jamschutz@users.noreply.github.com>
2025-04-15 14:45:20 -0400

    update dir

--- server/DataAPIs/publish.sh
+++ server/DataAPIs/publish.sh
@@ -1,2 +1,2 @@
-export DOTNET_BUNDLE_EXTRACT_BASE_DIR='/srv/backend/dotnet'
+export DOTNET_BUNDLE_EXTRACT_BASE_DIR='/srv/backend/article-apis/bin'
 dotnet publish --configuration Release -r linux-x64 -o /srv/backend/article-apis
