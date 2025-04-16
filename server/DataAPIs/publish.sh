ab83402fb0511c5ec8f7612f08befb3e6cf38c5c
jamschutz <52084893+jamschutz@users.noreply.github.com>
2025-04-16 15:53:20 -0400

    just copy contents of dir

--- server/DataAPIs/publish.sh
+++ server/DataAPIs/publish.sh
@@ -1,2 +1,2 @@
 dotnet publish --configuration Release -r linux-x64
-cp -rf /home/vga/the-videogame-archive/server/DataAPIs/bin/Release/net9.0/linux-x64 /srv/backend/article-apis
+cp -rf /home/vga/the-videogame-archive/server/DataAPIs/bin/Release/net9.0/linux-x64/* /srv/backend/article-apis
