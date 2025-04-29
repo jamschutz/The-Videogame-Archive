c150eefa107589b40cc4261e2dafab216a275f93
jamschutz <52084893+jamschutz@users.noreply.github.com>
2025-04-29 18:17:00 -0400

    update prod config

--- website/config/prod.js
+++ website/config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
     IMG_BASE_URL: "/archive/img",
 
     FILTER_CACHE_ID: 'dataFilters',
-    TARGET_DATES_CACHE_ID: 'targetDates'
+    TARGET_DATES_CACHE_ID: 'targetDates',
+    WEBSITE_ORDER_CACHE_ID: 'websiteOrder'
 };
