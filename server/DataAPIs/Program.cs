58bef65df3a5bd4fd4756df63bebd1616c7317f8
jamschutz <52084893+jamschutz@users.noreply.github.com>
2025-05-04 16:18:02 -0400

    please work...

--- server/DataAPIs/Program.cs
+++ server/DataAPIs/Program.cs
@@ -2,13 +2,13 @@ var AllowLocalConnections = "allowLocalConnections";
 var builder = WebApplication.CreateBuilder(args);
 
 // parse client id and secret from arguments
-if(args.Length < 4) {
+if(args.Length != 1 || args[0].Split('_').Length < 4) {
     throw new System.Exception($"ERROR: expected 4 arguments, but only got ${args.Length}. please ensure to have the following arguments in the following order: client-id, client-secret, project-id, environment");
 }
-string clientId = args[0];
-string clientSecret = args[1];
-string projectId = args[2];
-string environment = args[3];
+string clientId = args[0].Split('_')[0];
+string clientSecret =  args[0].Split('_')[1];
+string projectId =  args[0].Split('_')[2];
+string environment =  args[0].Split('_')[3];
 
 // Add services to the container.
 
--- server/run_backend.bat
+++ server/run_backend.bat
@@ -7,6 +7,6 @@ set environment=dev
 :: file host
 wt -w 0 -p "Powershell" --title filehost -d F:/VideogameArchive/_website_backups python -m http.server 5001
 :: search apis
-wt -w 0 -p "Powershell" --title search-apis -d "./DataAPIs" dotnet run %client-id% %client-secret% %project-id% %environment%
+wt -w 0 -p "Powershell" --title search-apis -d "./DataAPIs" dotnet run %client-id%_%client-secret%_%project-id%_%environment%
 :: open website tab
 wt -w 0 -p "Powershell" --title website -d "../website"
