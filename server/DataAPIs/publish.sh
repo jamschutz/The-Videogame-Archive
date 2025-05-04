817578164fae4aeff64a7a3a92fb9682474a38e7
jamschutz <52084893+jamschutz@users.noreply.github.com>
2025-05-04 12:29:49 -0400

    working on parsing arguments in shell

--- server/DataAPIs/publish.sh
+++ server/DataAPIs/publish.sh
@@ -1,2 +1,43 @@
-dotnet publish --configuration Release -r linux-x64
-cp -rf /home/vga/the-videogame-archive/server/DataAPIs/bin/Release/net9.0/linux-x64/* /srv/backend/article-apis
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -c|--client-id)
+      CLIENT_ID="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -s|--client-secret)
+      CLIENT_SECRET="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -p|--project-id)
+      PROJECT_ID=YES
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+echo "CLIENT ID  = ${CLIENT_ID}"
+echo "CLIENT SECRET     = ${CLIENT_SECRET}"
+echo "PROJECT ID         = ${PROJECT_ID}"
+
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 "$1"
+fi
+
+
+# dotnet publish --configuration Release -r linux-x64
+# cp -rf /home/vga/the-videogame-archive/server/DataAPIs/bin/Release/net9.0/linux-x64/* /srv/backend/article-apis
