325dcc94783859f71cd67367823198e5767fb527
joey schutz <schutzjoey@gmail.com>
2025-05-08 21:54:37 -0400

    created dockerfile to build and deploy app

--- server/DataAPIs/publish.sh
+++ server/DataAPIs/publish.sh
@@ -1,2 +1,18 @@
-dotnet publish --configuration Release -r linux-x64 
-cp -rf /home/vga/the-videogame-archive/server/DataAPIs/bin/Release/net9.0/linux-x64/* /srv/backend/article-apis
+# read environment from commandline
+env=$1
+if [[ "$env" == "" ]]; then
+    env='dev'
+fi
+
+# set target dir based on environment
+if [[ "$env" == "prod" ]]; then
+    dest='/srv/backend/article-apis'
+else
+    dest='../../../srv/backend/article-apis'
+fi
+
+# build 
+dotnet publish --configuration Release -r linux-x64
+
+# and publish to dest
+cp -rf ./bin/Release/net9.0/linux-x64/* $dest
